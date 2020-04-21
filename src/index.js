782859211dc6e5dd3d7eccf033c046a10fe822db
Andre Wiggins <andrewiggins@live.com>
2020-04-20 18:30:37 -0700

    Strip core of all unnecessary features to benchmark & improve core algorithm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-export { render, hydrate } from './render';
-export {
-	createElement,
-	createElement as h,
-	Fragment,
-	createRef,
-	isValidElement
-} from './create-element';
+export { render /* hydrate */ } from './render';
+export { createElement, createElement as h, Fragment } from // createRef,
+// isValidElement
+'./create-element';
 export { Component } from './component';
-export { cloneElement } from './clone-element';
-export { createContext } from './create-context';
-export { toChildArray } from './diff/children';
+// export { cloneElement } from './clone-element';
+// export { createContext } from './create-context';
+// export { toChildArray } from './diff/children';
 export { unmount as _unmount } from './diff';
-export { default as options } from './options';
+// export { default as options } from './options';
